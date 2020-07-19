185425da06a5bfa22b934f3d42de925e15f3c03f
shaunyoung-11 <shaun@std.uestc.edu.cn>
2020-07-19 17:01:16 +0800

    登录页面基本完成

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
 
-Vue.config.productionTip = false
+//导入全局样式表
+import "./assets/css/global.css";
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
