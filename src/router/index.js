606f9f03725bd5f0e53198c4d21def6f1954dee7
shaunyoung-11 <shaun@std.uestc.edu.cn>
2020-07-19 17:01:16 +0800

    登录页面基本完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "../components/login.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: "/", redirect: "/login" },
+    { path: "/login", component: Login }
   ]
-})
+});
