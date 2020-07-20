83aa01eab2821915060f577efb9ae2780e641230
shaunyoung-11 <shaun@std.uestc.edu.cn>
2020-07-20 16:32:50 +0800

    更改element.js

--- src/plugin/element.js
+++ src/plugin/element.js
@@ -1,12 +1,9 @@
-import Vue from "vue";
-import { Button } from "element-ui";
-import { Form, FormItem } from "element-ui";
-import { Input } from "element-ui";
-import { Message } from "element-ui";
-import "../../node_modules/element-ui/lib/theme-chalk/index.css";
+import Vue from 'vue'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import '../../node_modules/element-ui/lib/theme-chalk/index.css'
 
-Vue.use(Button);
-Vue.use(Form);
-Vue.use(FormItem);
-Vue.use(Input);
-Vue.prototype.$message = Message;
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
