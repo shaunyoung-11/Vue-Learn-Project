03698efce6ab59d1d00129aebe9650c664356274
shaunyoung-11 <shaun@std.uestc.edu.cn>
2020-07-20 22:19:24 +0800

    用户数据查询，添加用户功能

--- src/main.js
+++ src/main.js
@@ -1,25 +1,31 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import App from "./App";
-import router from "./router";
-import "./plugin/element";
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import './plugin/element'
 //导入图标
-import "./assets/font-awesome-4.7.0/css/font-awesome.css";
+import './assets/font-awesome-4.7.0/css/font-awesome.css'
 //导入全局样式表
-import "./assets/css/global.css";
+import './assets/css/global.css'
 
-import axios from "axios";
+import axios from 'axios'
 //配置请求根路径
-axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
-Vue.prototype.$http = axios;
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  //最后必须 return config
+  return config
+})
+Vue.prototype.$http = axios
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
+  el: '#app',
   router,
   components: { App },
-  template: "<App/>"
-});
+  template: '<App/>'
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,27 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Login from "../components/login.vue";
-import Home from "../components/home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../components/login.vue'
+import Home from '../components/home.vue'
+import Welcome from '../components/welcome.vue'
+import Users from '../components/user/users.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 const router = new Router({
   routes: [
-    { path: "/", redirect: "/login" },
-    { path: "/login", component: Login },
-    { path: "/home", component: Home }
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users }
+      ]
+    }
   ]
-});
+})
 
 //挂载路由导航守卫
 router.beforeEach((to, from, next) => {
@@ -19,11 +29,11 @@ router.beforeEach((to, from, next) => {
   //from 从哪个路径跳转而来
   //next 表示放行
 
-  if (to.path == "/login") return next();
+  if (to.path == '/login') return next()
   //获取token
-  const tokenStr = window.sessionStorage.getItem("token");
-  if (!tokenStr) return next("/login");
-  next();
-});
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 
-export default router;
+export default router
